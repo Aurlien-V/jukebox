bab615ac961002709a303c02a0f7a338e82cbb58
Aurlien-V <153271619+Aurlien-V@users.noreply.github.com>
2024-05-04 22:49:26 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,7 @@ const elements = {
     lecteur: document.querySelector("#lecteur"),
     cover: document.getElementById("cover"),
     randomButton: document.getElementById("randomButton"),
-    disque: document.querySelector(".disque") // Ajouté ici
+    disque: document.querySelector(".disque")
 };
 
 // Configuration des URLs pour les covers et les musiques
@@ -19,57 +19,33 @@ let lastPlayed = null;
 
 // Fonction pour récupérer les données à partir du fichier JSON
 async function fetchMusicData() {
-    try {
-        const response = await fetch("https://api2-6seh.onrender.com/api/v1/musics/");
-        const dbMusic = await response.json(); // Utilisation de response.json() au lieu de req.json()
-        data = dbMusic.result;
-        data.forEach((music) => {
-            playlist.innerHTML += `<li id="${music.id}"><h2>${music.title}</h2><div><small>${music.category}</small></div></li>`;
-        });
-
-        const allLi = document.querySelectorAll("li");
-
-        allLi.forEach((li) => {
-            li.addEventListener("click", function (elem) {
-                const id = parseInt(li.id);
-                const searchById = data.find((element) => element.id === id);
-                lecteur.src = `${config.urlSound}${searchById.sound}`;
-                lecteur.play();
-                cover.src = `${config.urlCover}${searchById.cover}`;
-                if (disque.classList.contains("pause")) {
-                    disque.classList.remove("pause");
-                }
-
-                allLi.forEach((item) => {
-                    item.classList.remove("clignote");
-                });
-
-                li.classList.add("clignote");
-            });
-
-        });
-    } catch (error) {
-        console.error('There was a problem with the fetch operation:', error);
-    }
+    const response = await fetch("https://api2-6seh.onrender.com/api/v1/musics/");
+    const dbMusic = await response.json();
+    data = dbMusic.result;
+    data.forEach((music) => {
+        const li = document.createElement("li");
+        li.textContent = music.title;
+        li.addEventListener("click", () => playMusic(music, li));
+        elements.playlist.appendChild(li);
+    });
 }
 
 // Fonction pour jouer une musique
 function playMusic(music, target) {
     elements.lecteur.src = `${config.urlSound}${music.sound}`;
-    elements.lecteur.play();
     elements.cover.src = `${config.urlCover}${music.cover}`;
+    elements.lecteur.play();
     // Retirer la classe 'playing' de tous les éléments de la playlist
     document.querySelectorAll("li").forEach(item => item.classList.remove('playing'));
     // Ajouter la classe 'playing' à l'élément cliqué
     target.classList.add('playing');
     // Faire tourner le vinyle
-    document.querySelector('.disque').classList.remove('pause');
+    elements.disque.classList.remove('pause');
 }
 
 // Fonction pour jouer une musique aléatoire
 function playRandomMusic() {
     let randomIndex;
-    // Générer un index aléatoire différent du dernier index joué
     do {
         randomIndex = Math.floor(Math.random() * data.length);
     } while (randomIndex === lastPlayed);
@@ -82,32 +58,18 @@ function playRandomMusic() {
 
 // Ajouter un écouteur d'événements au lecteur pour mettre en pause le vinyle lorsque la musique est en pause
 elements.lecteur.addEventListener('pause', function() {
-    document.querySelector('.disque').classList.add('pause');
+    elements.disque.classList.add('pause');
 });
 
 // Ajouter un écouteur d'événements au lecteur pour faire tourner le vinyle lorsque la musique reprend
 elements.lecteur.addEventListener('play', function() {
-    document.querySelector('.disque').classList.remove('pause');
+    elements.disque.classList.remove('pause');
 });
-
-// Ajouter un écouteur d'événements au lecteur pour jouer une nouvelle musique lorsque la musique actuelle est terminée
-elements.lecteur.addEventListener('ended', playRandomMusic);
 
 // Ajouter un écouteur d'événements au bouton "Musique Aléatoire"
 elements.randomButton.addEventListener("click", playRandomMusic);
 
 // Point d'entrée
 (async () => {
-    // Récupérer les données
-    data = await fetchMusicData();
-    
-    // Ajouter les musiques à la playlist
-    data.forEach((music) => {
-        const li = document.createElement("li");
-        li.textContent = music.title;
-        li.addEventListener("click", (event) => {
-            playMusic(music, event.target);
-        });
-        elements.playlist.appendChild(li);
-    });
+    await fetchMusicData();
 })();
